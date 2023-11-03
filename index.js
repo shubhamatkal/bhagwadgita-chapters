289cc0f2fe3534c78bb055910e775bc64b7574a9
shubhamatkal <shubhamatkal333@gmail.com>
2023-11-03 22:38:18 +0530

    handle get request

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ import axios from "axios";
 
 const app = express();
 const  port = 3000;
-// const url = oikj
+const chapter = 1;
+const config = {
+    headers: {
+      'X-RapidAPI-Key': '622e9946f6msh59b60898272652ep1d9ddajsn8dd7227eacc9',
+      'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
+    }
+  };
 
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
-    res.render("index.ejs")
+app.get("/", async (req, res)=>{
+    try {
+        const chapter = 1;
+        const response = await axios.get(`https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${chapter}/`,config);
+        // console.log(JSON.stringify(response.data.chapter_number));
+        res.render("index.ejs", { chapter_num: JSON.stringify(response.data.chapter_number),title: JSON.stringify(response.data.name_translated),summary: JSON.stringify(response.data.chapter_summary)});
+    }catch (error) {
+        res.render("index.ejs", { summary: JSON.stringify(error.response.data) });
+    }
+    
 });
 
 
