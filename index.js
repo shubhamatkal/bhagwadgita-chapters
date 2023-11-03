ea5db66bf406ed2d7e92c0d45616c18132263010
shubhamatkal <shubhamatkal333@gmail.com>
2023-11-03 16:21:07 +0530

    done for the day

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import axios from "axios";
 
 const app = express();
 const  port = 3000;
+const url = oikj
 
+app.use(express.static("public"));
 
+app.get("/", async (req, res)=>{
+    try {
+        // const result = await axios.get(url)
+    }
+});
 
 
 // end code
